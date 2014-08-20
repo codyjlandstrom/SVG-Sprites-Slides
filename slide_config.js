1e9a79de06b6472db171520f97060b2fb67abf8e
Cody J Landstrom <cody@dbox.com>
2014-08-19 23:38:28 -0400

    add twitter handle in config and add more content

--- slide_config.js
+++ slide_config.js
@@ -12,7 +12,7 @@ var SLIDE_CONFIG = {
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/nychtml5.svg',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -23,7 +23,7 @@ var SLIDE_CONFIG = {
   // Author information
   presenters: [{
     name: 'Cody J Landstrom',
-    company: 'DBOX',
+    company: '@codyjlandstrom',
     gplus: 'https://plus.google.com/u/0/+CodyJLandstrom/',
     twitter: '@codyjlandstrom',
     www: 'http://landstrom.co',
